6f5a02e40c74e3f3dc08d3705badd29648098bf6
slide-t <slideteckacademy@gmail.com>
2025-06-29 14:50:45 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('cards-store').then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'service-worker.js',
+        'icons/icon-192.png',
+        'icons/icon-512.png'
+      ]);
+    })
+  );
+});
 
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
