edd020713060b335b17f7ad5c9f51e44fd58c2a9
slide-t <slideteckacademy@gmail.com>
2025-06-29 14:52:51 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,1 +1,21 @@
-
+{
+  "name": "بطاقات عربية",
+  "short_name": "بطاقات",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#ffffff",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
